a36307c9fc742d074cfbdce6f5a75137ac6c14ef
didinele <didinele.dev@gmail.com>
2022-08-11 17:05:12 +0300

    chore: init website

---
+++ packages/frontend/next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+	reactStrictMode: true,
+	swcMinify: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	cleanDistDir: true,
+};
