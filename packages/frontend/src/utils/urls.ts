ea218d73918f73d10706117e57f245f29b44e0cc
didinele <didinele.dev@gmail.com>
2022-09-17 19:24:56 +0300

    feat: vercel url support

--- packages/frontend/src/utils/urls.ts
+++ packages/frontend/src/utils/urls.ts
@@ -1,2 +1,2 @@
-export const LogIn = `${process.env.NEXT_PUBLIC_API_URL!}/auth/v1/discord?redirect_uri=${process.env
-	.NEXT_PUBLIC_SITE_URL!}`;
+export const LogIn = `${process.env.NEXT_PUBLIC_API_URL!}/auth/v1/discord?redirect_uri=${(process.env
+	.NEXT_PUBLIC_VERCEL_URL ?? process.env.NEXT_PUBLIC_SITE_URL)!}`;
