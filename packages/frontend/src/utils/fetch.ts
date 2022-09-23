a4b90d8c9884d6411f61df138eaad1fbc06e5e16
Tuur Martens <tuurmartens4@gmail.com>
2022-09-23 21:53:39 +0200

    fix(frontend): make suggested changes

--- packages/frontend/src/utils/fetch.ts
+++ packages/frontend/src/utils/fetch.ts
@@ -26,7 +26,7 @@ export async function fetchApi<TPath extends keyof AuthRoutes, TMethod extends k
 	});
 
 	if (res.status >= 200 && res.status < 300) {
-		return (await res.json()) as InferAuthRouteResult<TPath, TMethod>;
+		return res.json() as Promise<InferAuthRouteResult<TPath, TMethod>>;
 	}
 
 	throw new APIError((await res.json()) as Payload);
