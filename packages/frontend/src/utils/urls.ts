4119ceaa5a025e771a66c545e6f66820c4c4f72d
didinele <didinele.dev@gmail.com>
2022-09-25 16:58:04 +0300

    fix(frontend/urls): deal with prod urls properly

--- packages/frontend/src/utils/urls.ts
+++ packages/frontend/src/utils/urls.ts
@@ -1,10 +1,8 @@
-// TODO: Redirect to /dashboard
-export const LogIn = `${process.env.NEXT_PUBLIC_API_URL!}/auth/v1/discord?redirect_uri=${(process.env
-	.NEXT_PUBLIC_VERCEL_URL
-	? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-	: process.env.NEXT_PUBLIC_SITE_URL)!}`;
+// NEXT_PUBLIC_SITE_URL is a variable we set both in prod and in dev
+// We only rely on NEXT_PUBLIC_VERCEL_URL in previews
+const Host = process.env.NEXT_PUBLIC_SITE_URL ?? `https://${process.env.NEXT_PUBLIC_VERCEL_URL!}`;
 
-export const LogOut = `${process.env.NEXT_PUBLIC_API_URL!}/auth/v1/logout?redirect_uri=${(process.env
-	.NEXT_PUBLIC_VERCEL_URL
-	? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-	: process.env.NEXT_PUBLIC_SITE_URL)!}`;
+// TODO: Redirect to /dashboard
+export const LogIn = `${process.env.NEXT_PUBLIC_API_URL!}/auth/v1/discord?redirect_uri=${Host}` as const;
+
+export const LogOut = `${process.env.NEXT_PUBLIC_API_URL!}/auth/v1/logout?redirect_uri=${Host}` as const;
