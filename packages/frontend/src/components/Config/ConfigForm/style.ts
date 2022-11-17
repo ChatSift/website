239e720c9640271448a0b3930cdb1def3fd9fea8
Tuur Martens <tuurmartens4@gmail.com>
2022-11-17 14:36:40 +0100

    fix dirty bar for mobile

--- packages/frontend/src/components/Config/ConfigForm/style.ts
+++ packages/frontend/src/components/Config/ConfigForm/style.ts
@@ -1,33 +1,25 @@
 import styled from '@emotion/styled';
 import { Body } from '~/components/Text';
-import mediaQueries from '~/styles/breakpoints';
 
 export const DirtyBar = styled.div`
-	position: absolute;
-	bottom: 16px;
-	left: 16px;
-	right: 16px;
+	position: sticky;
+	bottom: 0;
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
 	align-items: center;
 	border: 1px solid ${({ theme }) => theme.colors.onBackground.secondary};
 	border-radius: 8px;
-	background-color: ${({ theme }) => theme.colors.onBackground.tertiary};
+	background-color: ${({ theme }) => theme.colors.background.card};
 	padding: 8px 16px;
 	transition: opacity 0.2s ease-in-out, transform 0.2s ease-in-out;
 	transform: translateY(0);
 	opacity: 1;
+	margin-top: auto;
 
 	&[data-hidden='true'] {
 		transform: translateY(10px);
 		opacity: 0;
-	}
-
-	${mediaQueries.smallMin} {
-		bottom: 24px;
-		left: 24px;
-		right: 24px;
 	}
 `;
 
