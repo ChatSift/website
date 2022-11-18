9695a48f9e22bad1a439ce57752397cc9ebb87d7
Tuur Martens <tuurmartens4@gmail.com>
2022-11-18 08:48:15 +0100

    Update packages/api/src/routes/auth/discordAuthMe.ts
    Co-authored-by: DD <didinele.dev@gmail.com>

--- packages/api/src/routes/auth/discordAuthMe.ts
+++ packages/api/src/routes/auth/discordAuthMe.ts
@@ -57,7 +57,8 @@ export default class extends Route<GetDiscordAuthMeResult, never> {
 			discriminator,
 			avatar,
 			guilds: await Promise.all(
-				// @ts-expect-error TS2339
+				// @ts-expect-error - Missing discord-api-types
+				// TODO: Remove once upstream has the types
 				guilds.map(async ({ id, name, icon, approximate_member_count, approximate_presence_count }) => {
 					const [hasAutomoderator, hasAma, hasModmail] = await Promise.all([
 						this.has(id, this.automoderatorRest),
