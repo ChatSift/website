8702fcb8acd53dbe06e409bad7e8a525e6e1bb02
Tuur Martens <tuurmartens4@gmail.com>
2022-12-03 13:02:40 +0100

    feat: simple 404 page (#24)

---
+++ packages/frontend/src/pages/404.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import * as Button from '~/components/Button';
+import Footer from '~/components/Footer';
+import { RouterLink } from '~/components/Link';
+import PageMeta from '~/components/PageMeta';
+
+const Container = styled.main`
+	flex: 1 0 auto;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	justify-content: center;
+`;
+
+const NotFoundTitle = styled.h3`
+	font-size: 100px;
+	font-weight: 550;
+	color: ${({ theme }) => theme.colors.text.primary};
+	display: flex;
+	flex-direction: row;
+	gap: 16px;
+	align-items: center;
+`;
+
+const HomeButton = Button.Cta.withComponent(RouterLink);
+
+function NotFound404() {
+	return (
+		<>
+			<PageMeta title="404 Not Found" />
+			<Container>
+				<NotFoundTitle>404</NotFoundTitle>
+				<HomeButton href="/">Go home</HomeButton>
+			</Container>
+			<Footer />
+		</>
+	);
+}
+
+export default NotFound404;
